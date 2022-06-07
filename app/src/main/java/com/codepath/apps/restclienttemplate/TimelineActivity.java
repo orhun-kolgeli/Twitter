fedec34d09b10e4732a73216ecc2b47f4d330ca7
Orhun Kolgeli <74283397+orhun-kolgeli@users.noreply.github.com>
2022-06-07 13:58:36 -0700

    Add menu item for user to compose new tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,8 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -67,6 +71,27 @@ public class TimelineActivity extends AppCompatActivity {
         populateHomeTimeline();
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_main.xml layout
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Return true in order for the menu to be displayed
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Check if the selected menu item is the compose menu item
+        if (item.getItemId() == R.id.compose) { // Compose icon is tapped
+            // Navigate to compose activity
+
+            // Return true to consume the tap of the menu item here
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateHomeTimeline() {
